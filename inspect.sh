7b0079eaf8242ef1765673f5a74568da0285fdd3
ATTY Lionel <lionel.atty@ign.fr>
2017-02-13 18:02:17 +0100

    modification de inspect.sh pour monter le volume vers les sources ARDUINO et acquisition du device /ttyACM0 (l'Arduino).

--- inspect.sh
+++ inspect.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+# url: http://unix.stackexchange.com/questions/23111/what-is-the-eval-command-in-bash
+CUR_PATH=$(realpath .)
+
+# url: https://docs.docker.com/engine/reference/commandline/run/
+
 #li3dsdockerarduino_arduino \
 docker run -it \
     --network=li3dsarduinodocker_default \
     --env ROS_HOSTNAME=arduino_client \
     --env ROS_MASTER_URI=http://master:11311 \
+    -v $CUR_PATH/arduino/li3ds-Arduino:/root/li3ds-Arduino \
+    --device=/dev/ttyACM0:/dev/ttyACM0 \
     $1 \
     /bin/bash
